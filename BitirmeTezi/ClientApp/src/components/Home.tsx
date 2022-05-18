72d97dfb9b7b807e77634575c3199320e9905fe2
Egecan Ceylan <egecanceylan99@gmail.com>
2022-05-19 01:44:25 +0300

    post request for login

--- BitirmeTezi/ClientApp/src/components/Home.tsx
+++ BitirmeTezi/ClientApp/src/components/Home.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Button, Input, ListGroupItemHeading, NavLink } from 'reactstrap'
-import { Link } from 'react-router-dom';
-import { pathToFileURL } from 'url';
+import { Button, Input } from 'reactstrap'
 
 type MyState = {email: string, password: string}
 
@@ -14,13 +12,21 @@ class Home extends React.Component<{}, MyState> {
     this.signUp = this.signUp.bind(this)
   }
 
+  componentWillMount() {
+    if (localStorage.getItem('token') !== null) {
+      console.log("not null");
+    } else {
+      console.log("null");
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <h1>Welcome to the World's Best Live Stream Platform!</h1>
         <div className='w-25 mt-4'>
           <Input placeholder='Email' onChange={(e) => this.setState({email: e.target.value})}></Input>
-          <Input placeholder='Parola' className='mt-4' onChange={(e) => this.setState({password: e.target.value})}></Input>
+          <Input placeholder='Parola' type='password' className='mt-4' onChange={(e) => this.setState({password: e.target.value})}></Input>
           <div className='row justify-content-around'>
             <Button className='mt-4 col-0 align-self-start' style={{background:'#A200C1', borderColor:'#A200C1'}} onClick={this.login}>Giriş Yap</Button>
             <Button className='mt-4 col-0 align-self-end' style={{background:'#A200C1', borderColor:'#A200C1'}} onClick={this.signUp}>Kayıt Ol</Button>
@@ -32,8 +38,32 @@ class Home extends React.Component<{}, MyState> {
   }
 
   login() {
-    console.log(this.state.email)
-    console.log(this.state.password)
+    var email = this.state.email.trim();
+    var password = this.state.password.trim();
+
+    var body = JSON.stringify({email: email, password: password});
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body : body
+    };
+
+    fetch('https://localhost:44306/api/auth/login', requestOptions)
+      .then(async response => {
+        const isJson = response.headers.get('content-type')!.includes('application/json');
+        const data = isJson && await response.json();
+
+        if (response.ok) {
+          console.log(data.id)  
+          console.log(data.email)  
+          console.log(data.username)  
+          console.log(data.token)  
+          localStorage.setItem('token', data.token)        
+        } else {
+          alert(data.Error[0])  
+        }
+      })      
   }
   
   signUp() {
