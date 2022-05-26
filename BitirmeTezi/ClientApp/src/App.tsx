e59ee4227f4651b0fc02c181fed6b01524d81552
Erdem Çobanoğlu <erdem.cobanoglu1635@gmail.com>
2022-05-27 01:03:53 +0300

    App.tsx tekrar düzeltildi.

--- BitirmeTezi/ClientApp/src/App.tsx
+++ BitirmeTezi/ClientApp/src/App.tsx
@@ -4,12 +4,10 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
-import VideoPlayer from './components/VideoPlayer';
 import './custom.css'
 
 export default () => (
     <Layout>
-        <Route exact path='/' component={VideoPlayer} />
         <Route path='/home' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
