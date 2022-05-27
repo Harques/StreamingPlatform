6a454415820f79b26c8f4e42ca869945def86bb4
Erdem Çobanoğlu <erdem.cobanoglu1635@gmail.com>
2022-05-28 02:11:52 +0300

    FFMPEG paketi eklendi.

---
+++ BitirmeTezi/Controllers/SubtitleController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BitirmeTezi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubtitleController : ControllerBase
+    {
+        // GET: api/<SubtitleController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SubtitleController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SubtitleController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SubtitleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SubtitleController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
