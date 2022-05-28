2bf588c2e85a11c54743a6164fe7cbd6f6ba3904
Erdem Çobanoğlu <erdem.cobanoglu1635@gmail.com>
2022-05-28 13:13:07 +0300

    FFmpeg denemeleri v1

--- BitirmeTezi/Controllers/SubtitleController.cs
+++ BitirmeTezi/Controllers/SubtitleController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Net;
+using Xabe.FFmpeg;
+using System.IO; 
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,9 +14,25 @@ namespace BitirmeTezi.Controllers
     {
         // GET: api/<SubtitleController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async void Get()
         {
-            return new string[] { "value1", "value2" };
+            //using (var client = new WebClient())
+            //{
+            //    string url = "http://20.54.150.204:8080/hls/test-20.ts";
+            //    await client.DownloadFileTaskAsync(new System.Uri(url), "file.ts");
+            //}
+            string currentDirectory = Directory.GetCurrentDirectory();
+            //string input = Path.Combine(Directory.GetParent(currentDirectory).FullName, "file.ts");
+            //string output = Path.Combine(Directory.GetParent(currentDirectory).FullName, "file.mp4");
+
+            //await FFmpeg.Conversions.FromSnippet.Convert(input, output);
+
+            var mediaInfo = await FFmpeg.GetMediaInfo(Path.Combine(currentDirectory,"file.ts"));
+            string output = Path.Combine(currentDirectory, "test.mp3");
+            var conversion = await FFmpeg.Conversions.FromSnippet.ExtractAudio(mediaInfo.Path, output);
+
+            //conversion.Start();
+
         }
 
         // GET api/<SubtitleController>/5
--- BitirmeTezi/Program.cs
+++ BitirmeTezi/Program.cs
@@ -4,8 +4,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Downloader;
 
 namespace BitirmeTezi
 {
@@ -13,7 +16,14 @@ namespace BitirmeTezi
     {
         public static void Main(string[] args)
         {
+            MainAsync(args).GetAwaiter().GetResult();
             CreateHostBuilder(args).Build().Run();
+
+        }
+        static async Task MainAsync(string[] args)
+        {
+            await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, "./FFmpeg");
+            FFmpeg.SetExecutablesPath("./FFmpeg");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
