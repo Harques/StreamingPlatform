5a7e45bcc0c73e4ed9e5c4232455b66a57ee9410
Erdem Çobanoğlu <erdem.cobanoglu1635@gmail.com>
2022-05-31 18:27:33 +0300

    Worker inaktif edildi.

--- BitirmeTezi/Startup.cs
+++ BitirmeTezi/Startup.cs
@@ -29,7 +29,7 @@ namespace BitirmeTezi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddHostedService<Worker>();
+            //services.AddHostedService<Worker>();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
