11b9b426585f235728899a2c6529611f18929ac0
Egecan Ceylan <egecanceylan99@gmail.com>
2022-06-01 11:31:39 +0300

    buffer array size changed

--- BitirmeTezi/WorkerService/Worker.cs
+++ BitirmeTezi/WorkerService/Worker.cs
@@ -73,7 +73,7 @@ namespace BitirmeTezi.WorkerService
                 int lastRead = 0;
                 bool firstTime = true;
 
-                byte[] buffer = new byte[17500];
+                byte[] buffer = new byte[32768];
                 do
                 {
                     lastRead = baseStream.Read(buffer, 0, buffer.Length);
