e5a46e2b37a58b1cf750e6903912e862fa2fea85
Erdem Çobanoğlu <erdem.cobanoglu1635@gmail.com>
2022-06-22 23:12:58 +0300

    Update Dashboard.tsx

--- BitirmeTezi/ClientApp/src/components/Dashboard.tsx
+++ BitirmeTezi/ClientApp/src/components/Dashboard.tsx
@@ -5,34 +5,41 @@ import { Context } from "../helpers/Context";
 import Row from "antd/lib/grid/row";
 import { Menu, Dropdown, Button, message, Tooltip } from "antd";
 import ArrowDropDownOutlinedIcon from "@material-ui/icons/ArrowDropDownOutlined";
+import { DH_CHECK_P_NOT_SAFE_PRIME } from "constants";
 
-type MyState = { url : string};
 type MyProps = {};
 
-const menu = (
-    <Menu onClick={handleMenuClick}>
-      <Menu.Item key='1'>Oyun</Menu.Item>
-      <Menu.Item key='2'>Sohbet</Menu.Item>
-      <Menu.Item key='3'>Eğitim</Menu.Item>
-    </Menu>
-  );
 
-function handleMenuClick(e: any) {
-    message.info("Category has been changed.");
-    console.log("click", e);
-}
-class Dashboard extends React.Component<MyProps, MyState> {
+
+class Dashboard extends React.Component<MyProps> {
     streamService: StreamService    
 
     constructor(props: any) {
         super(props)
         this.streamService = new StreamService()
         this.checkIfStreamOpened()
-    }
+        this.state = {category : "Yayın Kategorisi"}
+        this.handleMenuClick = this.handleMenuClick.bind(this)
+      }
+      handleMenuClick = (e : any) => {
+        this.setState({category: this.menu.props.children[e.key-1].props.children})
+      }
+      menu = (
+        <Menu onClick={this.handleMenuClick}>
+          <Menu.Item key='1'>Oyun</Menu.Item>
+          <Menu.Item key='2'>Sohbet</Menu.Item>
+          <Menu.Item key='3'>Eğitim</Menu.Item>
+        </Menu>
+      );
+
 
     render() {
         return(
             <>
+            <Row style={{ width: "100%", justifyContent: "center" }}>
+              <h2
+              >Yayın Kontrol Paneli</h2>
+            </Row>
             <Row
               style={{
                 width: "100%",
@@ -41,9 +48,9 @@ class Dashboard extends React.Component<MyProps, MyState> {
               }}
             >
               <div id='components-dropdown-demo-dropdown-button'>
-                <Dropdown overlay={menu}>
+                <Dropdown overlay={this.menu}>
                   <Button size='large'>
-                    Yayın Kategorisi
+                    {this.state.category}
                     <ArrowDropDownOutlinedIcon />
                   </Button>
                 </Dropdown>
@@ -58,7 +65,7 @@ class Dashboard extends React.Component<MyProps, MyState> {
             var bool = await this.streamService.checkIfStreamOpened()
             console.log(bool)
             if (bool) {
-                this.streamService.startStream('Gaming')
+                this.streamService.startStream(this.state.category)
                 clearInterval(autoSaveInterval)                
             }
         }, 1000);
